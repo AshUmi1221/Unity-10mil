bfd86f1c92bd73287d7fbcd5e03ebb27b8f29fb2
AshUmi1221 <150216297+AshUmi1221@users.noreply.github.com>
2023-11-08 18:16:06 -0800

    Jump

--- Walter Jumper/Assets/JesseScript.cs
+++ Walter Jumper/Assets/JesseScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class JesseScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody.velocity = Vector2.up * 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * 10;
+        }
+       
     }
 }
